1e24e1e649843602aee7540e525acba8d192a9f7
Louis Collet <colletlouis.dev@gmail.com>
2024-10-03 16:46:58 +0200

    Added images and style

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import Image from 'next/image'
+import styles from './about.module.css'
 
 const AboutPage = () => {
-  return <div>AboutPage</div>
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src='/about.png'
+          alt='about'
+          fill
+        />
+      </div>
+    </div>
+  )
 }
 
 export default AboutPage
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import styles from './footer.module.css'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Antropom</div>
+      <div className={styles.text}>Antropom creative agency</div>
+    </div>
+  )
 }
 
 export default Footer
