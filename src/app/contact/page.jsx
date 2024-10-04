d8092a47da065c8aba36341d0ffcec004494493b
Louis Collet <colletlouis.dev@gmail.com>
2024-10-04 14:43:12 +0200

    Added contact page style

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import Image from 'next/image'
+import styles from './contact.module.css'
 
 const ContactPage = () => {
-  return <div>ContactPage</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          className={styles.img}
+          src='/contact.png'
+          alt='Contact image'
+          fill
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <form
+          action=''
+          className={styles.form}
+        >
+          <input
+            type='text'
+            placeholder='Name and Surname'
+          />
+          <input
+            type='text'
+            placeholder='Email address'
+          />
+          <input
+            type='text'
+            placeholder='Phone number (optional)'
+          />
+          <textarea
+            name=''
+            id=''
+            cols='30'
+            rows='10'
+            placeholder='Message'
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default ContactPage
