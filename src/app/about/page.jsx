708d48a28c0b1dd45bbbe74cb977743c63f5c1b7
Louis Collet <colletlouis.dev@gmail.com>
2024-10-04 14:28:48 +0200

    Added styles to about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,11 +3,38 @@ import styles from './about.module.css'
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod illum
+          doloribus tempora facilis, repellat magni assumenda maxime? Corrupti
+          aperiam ut dolorum, debitis commodi aut ullam labore consectetur
+          recusandae beatae animi.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
         <Image
+          className={styles.img}
           src='/about.png'
-          alt='about'
+          alt='About image'
           fill
         />
       </div>
